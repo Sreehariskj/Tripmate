64e9cea3c4347f4d82af3698e9fa079b0e470f21
sreehari-cyberonics <sreeharicyberonics@gmail.com>
2024-06-18 21:16:19 +0530

    core: homecard double tap like animation

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -7,76 +7,118 @@ import {
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {AppText} from './ui/AppText';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {ms} from '../helper/responsive';
 import {COLORS} from '../constants/colors';
 import {FONT_SIZE, FONT_WEIGHT, SPACING} from '../constants/sizes';
 import {BlurView} from '@react-native-community/blur';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSpring,
+} from 'react-native-reanimated';
+import {
+  GestureHandlerRootView,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
 
 interface BannerCardProps {
   item: any;
+  onPress: () => void;
   style: StyleProp<ImageStyle>;
 }
 
 const LikeIconSize = ms(20);
 const LikeIconContainerSize = ms(LikeIconSize + 10);
 export const HomeCard = ({item, style}: BannerCardProps) => {
-  const {id, image, isLiked, onPress} = item;
+  const {id, image, isLiked} = item;
+  const IconComponent = Animated.createAnimatedComponent(Icon);
+  const scale = useSharedValue(0);
+  const doubleTap = useCallback(() => {
+    scale.value = withSpring(1, undefined, isFinished => {
+      scale.value = withDelay(100, withSpring(0));
+    });
+  }, []);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: Math.max(scale.value, 0)}],
+    };
+  });
   return (
-    <TouchableWithoutFeedback onPress={onPress} style={[styles.container]}>
-      <ImageBackground
-        source={image}
-        style={[styles.imageContainer, style]}
-        // resizeMode="cover"
-      >
-        <View style={styles.top}>
-          <View style={styles.circle}>
-            <Icon name="heart" size={LikeIconSize} color="red" />
-          </View>
-        </View>
-        <View style={styles.bottom}>
-          <BlurView
-            style={styles.absolute}
-            blurType="light"
-            blurAmount={10}
-            reducedTransparencyFallbackColor="white"
-          />
-          <View style={styles.bottomLeft}>
-            <AppText style={[styles.bottomText, styles.name]}>
-              {item.name}
-            </AppText>
-            <View style={[styles.iconTextBox]}>
-              <Icon
-                name="map-marker-outline"
-                size={ms(18)}
+    <TouchableWithoutFeedback style={[styles.container]}>
+      <GestureHandlerRootView>
+        <TapGestureHandler
+          maxDelayMs={250}
+          numberOfTaps={2}
+          onActivated={doubleTap}>
+          <Animated.View style={{flex: 1}}>
+            <ImageBackground
+              source={image}
+              style={[styles.imageContainer, style]}
+              // resizeMode="cover"
+            >
+              <View style={styles.top}>
+                <View style={styles.circle}>
+                  <Icon
+                    name={isLiked ? 'heart' : 'heart-outline'}
+                    size={LikeIconSize}
+                    color="red"
+                  />
+                </View>
+              </View>
+              <View style={styles.bottom}>
+                <BlurView
+                  style={styles.absolute}
+                  blurType="light"
+                  blurAmount={10}
+                  reducedTransparencyFallbackColor="white"
+                />
+                <View style={styles.bottomLeft}>
+                  <AppText style={[styles.bottomText, styles.name]}>
+                    {item.name}
+                  </AppText>
+                  <View style={[styles.iconTextBox]}>
+                    <Icon
+                      name="map-marker-outline"
+                      size={ms(18)}
+                      color={COLORS.LIGHT}
+                      style={[styles.locationIcon]}
+                    />
+                    <AppText style={[styles.bottomText, styles.locationText]}>
+                      {item.location}
+                    </AppText>
+                  </View>
+                </View>
+                <View style={[styles.bottomRight]}>
+                  <AppText style={[styles.bottomText, styles.name]}>
+                    {`${item.rate.currency}${item.rate.value}`}
+                  </AppText>
+                  <View style={[styles.iconTextBox]}>
+                    <Icon
+                      name="star"
+                      size={ms(18)}
+                      color={'gold'}
+                      style={[styles.locationIcon]}
+                    />
+                    <AppText style={[styles.bottomText, styles.ratingText]}>
+                      {item.rating}
+                    </AppText>
+                  </View>
+                </View>
+              </View>
+              <IconComponent
+                name="heart"
                 color={COLORS.LIGHT}
-                style={[styles.locationIcon]}
+                size={ms(100)}
+                style={[styles.doubleTap, animatedStyle]}
               />
-              <AppText style={[styles.bottomText, styles.locationText]}>
-                {item.location}
-              </AppText>
-            </View>
-          </View>
-          <View style={[styles.bottomRight]}>
-            <AppText style={[styles.bottomText, styles.name]}>
-              {`${item.rate.currency}${item.rate.value}`}
-            </AppText>
-            <View style={[styles.iconTextBox]}>
-              <Icon
-                name="star"
-                size={ms(18)}
-                color={'gold'}
-                style={[styles.locationIcon]}
-              />
-              <AppText style={[styles.bottomText, styles.ratingText]}>
-                {item.rating}
-              </AppText>
-            </View>
-          </View>
-        </View>
-      </ImageBackground>
+            </ImageBackground>
+          </Animated.View>
+        </TapGestureHandler>
+      </GestureHandlerRootView>
     </TouchableWithoutFeedback>
   );
 };
@@ -129,6 +171,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: 'yellow',
+  },
+  doubleTap: {
+    position: 'absolute',
+    top: '30%',
+    left: '30%',
   },
   iconTextBox: {
     flexDirection: 'row',
