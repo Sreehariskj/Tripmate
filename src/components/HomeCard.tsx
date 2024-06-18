6f0376058167c8ca2cf2ff6b42b1432d761f43b8
sreehari-cyberonics <sreeharicyberonics@gmail.com>
2024-06-18 21:39:48 +0530

    fix: red color on homecard on inital load of app

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -198,7 +198,7 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
-    backgroundColor: 'red',
+    // backgroundColor: 'red',
     justifyContent: 'space-between',
   },
   locationIcon: {},
